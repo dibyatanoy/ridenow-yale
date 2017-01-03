7fd06fb368d956bacfb2c6c34a32fa05df0f77be
dibyatanoy <dibyatanoy.bhattacharjee@yale.edu>
2017-01-03 12:45:37 +0600

    Added business logic for finding best routes

--- index.js
+++ index.js
@@ -3,10 +3,14 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
+const async = require('async')
 const app = express()
 const fbToken = process.env.RIDE_NOW_FB_TOKEN
 const gmapToken = process.env.RIDENOW_GMAPS_GEOCODING_TOKEN
 const translocKey = process.env.RIDENOW_TRANSLOC_API_KEY
+const yaleAgencyId = '128'
+const walkTimeTolerance = 60 // in seconds
+var moment = require('moment')
 
 app.set('port', (process.env.PORT || 5000))
 
@@ -38,8 +42,9 @@ app.post('/webhook/', function (req, res) {
             let text = event.message.text
             //sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
             console.log("text received")
-            geoCode(text + ", New Haven, CT")
-            getRoutes('yale')
+            //geoCode(text + ", New Haven, CT")
+            //getRoutes('yale')
+            downloadRoutesAndContinue('yale', {lat: 41.3102168, lng: -72.93068079999999}, {lat: 41.3125884, lng: -72.92496140000002})
         }else if (event.message && event.message.attachments){
 
             if(event.message.attachments[0].payload.coordinates){
@@ -112,10 +117,10 @@ function getRoutes(agency){
         qs: {
             agencies: agency,
         },
-        method: 'GET'
+        method: 'GET',
     }, function(error, response, body){
         if (error) {
-            console.log('Error sending messages: ', error)
+            console.log('Error getting routes: ', error)
         } else if (response.body.error) {
             console.log('Error: ', response.body.error)
         }else{
@@ -124,6 +129,278 @@ function getRoutes(agency){
             //console.log(response.body)
         }
     })
+}
+
+var routesAndClosestStopsWithArrivals = []
+var routesAndClosestStops = []
+var walkDistances = {}
+var stopDescs = []
+
+function calcWalkDistanceToStop(routeid, stopid, src, dest, mcb){
+
+    var numStops = stopDescs.length
+
+    for(var i = 0; i < numStops; i++){
+        var stopDesc = stopDescs[i]
+
+        if(stopDesc.stop_id == stopid){
+            var stopLoc = stopDesc.location
+
+            request({
+                url: 'http://maps.googleapis.com/maps/api/distancematrix/json',
+                qs:{
+                    origins: src.lat.toString() + ',' + src.lng.toString(),
+                    destinations: stopLoc.lat.toString() + ',' + stopLoc.lat.toString(),
+                    key: gmapToken,
+                    mode: 'walking',
+                },
+                method: 'GET',
+            }, function(error, response, body){
+                if (error) {
+                    console.log('Error getting distance matrix: ', error)
+                } else if (response.body.error) {
+                    console.log('Error: ', response.body.error)
+                }else{
+                    var walkTimeSrc = JSON.parse(body).rows[0].elements[0].duration.value
+                    //walkDistances[routeid].push({stopid: stopid, walkTime: walkTimeSrc})
+
+                    request({
+                        url: 'http://maps.googleapis.com/maps/api/distancematrix/json',
+                        qs:{
+                            destinations: dest.lat.toString() + ',' + dest.lng.toString(),
+                            origins: stopLoc.lat.toString() + ',' + stopLoc.lat.toString(),
+                            key: gmapToken,
+                            mode: 'walking',
+                        },
+                        method: 'GET',
+                    }, function(error, response, body){
+                        if (error) {
+                            console.log('Error getting distance matrix: ', error)
+                        } else if (response.body.error) {
+                            console.log('Error: ', response.body.error)
+                        }else{
+                            var walkTimeDest = JSON.parse(body).rows[0].elements[0].duration.value
+                            walkDistances[routeid].push({stopid: stopid, walkTimeSrc: walkTimeSrc, walkTimeDest: walkTimeDest})
+
+                            mcb()
+                        }
+                    })
+                    //mcb()
+                }
+            })
+
+            break
+        }
+    }
+
+    return
+}
+
+function getBestStops(route, src, dest, mcb){
+
+    var asyncTasksStops = []
+
+    var routeId = route.routeId
+    var stops = route.stops
+    walkDistances[routeId] = []
+
+    stops.forEach(function(stopid){
+        asyncTasksStops.push(function(callback){
+            //business logic
+            calcWalkDistanceToStop(routeId, stopid, src, dest, function(){
+                callback()
+            })
+        })
+    })
+
+    async.parallel(asyncTasksStops, function(){
+
+        // walkDistances[routeId] has been populated, find minimum to source and destination
+        if (!routeId in walkDistances){
+            console.log("Error: routeId not in walkDistances")
+        }else{
+
+            var closestStopInfo = {routeId: routeId, routeName: route.long_name, closestToSrc: null, closestToDest: null, 
+                minDistSrc: null, minDistDest: null}
+
+            var numStopsOnRoute = walkDistances[routeId].length
+            var closestToSrc = null, closestToDest = null
+            var closestToSrcIdx = -1
+            var minDistSrc = 1000000
+            var minDistDest = 1000000
+
+            for(var i = 0; i < numStopsOnRoute; i++){
+                if (walkDistances[routeId][i].walkTimeSrc < minDistSrc){
+                    minDistSrc = walkDistances[routeId][i].walkTimeSrc
+                    closestToSrc = walkDistances[routeId][i].stopid
+                    closestToSrcIdx = i
+                }
+            }
+
+            for(var i = 0; i < numStopsOnRoute; i++){
+                if(walkDistances[routeId][i].walkTimeDest < minDistDest && i != closestToSrcIdx){
+                    minDistDest = walkDistances[routeId][i].walkTimeDest
+                    closestToDest = walkDistances[routeId][i].stopid
+                }
+            }
+
+            closestStopInfo.closestToSrc = closestToSrc
+            closestStopInfo.closestToDest = closestToDest
+            closestStopInfo.minDistSrc = minDistSrc
+            closestStopInfo.minDistDest = minDistDest
+
+            routesAndClosestStops.push(closestStopInfo)
+
+        }
+
+        mcb()
+    })
+}
+
+function getClosestStopsAllRoutes(routes, src, dest){
+
+    var asyncTasksRoutes = []
+
+    routes.forEach(function(route){
+        asyncTasksRoutes.push(function(callback){
+            //asyncCall for stops
+            getBestStops(route, src, dest, function(){
+                callback()
+            })
+        })
+    })
+
+    async.parallel(asyncTasksRoutes, function(){
+        //do something with closest stops for each route
+        console.log('Downloaded closest stops for each route')
+        console.log(routesAndClosestStops)
+        getStopArrivalTimes(src, dest)
+    })
+}
+
+function downloadRoutesAndContinue(agency, src, dest){
+
+    request({
+        headers: {
+            'X-Mashape-Key': translocKey,
+            'Accept': 'application/json',
+        },
+        url: 'https://transloc-api-1-2.p.mashape.com/routes.json',
+        qs: {
+            agencies: agency,
+        },
+        method: 'GET',
+    }, function(error, response, body){
+        if (error) {
+            console.log('Error getting stop descriptions: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }else{
+            //console.log('lat: %f, long: %f', JSON.parse(body).results[0].geometry.location.lat, JSON.parse(body).results[0].geometry.location.lng)
+            var routes = JSON.parse(body).data[yaleAgencyId]
+            console.log("Downloaded route information")
+            downloadStopDescsAndContinue(agency, routes, src, dest)
+        }
+    })
+}
+
+function downloadStopDescsAndContinue(agency, routes, src, dest){
+
+    //save .data part to stopDescs
+
+    request({
+        headers: {
+            'X-Mashape-Key': translocKey,
+            'Accept': 'application/json',
+        },
+        url: 'https://transloc-api-1-2.p.mashape.com/stops.json',
+        qs: {
+            agencies: agency,
+        },
+        method: 'GET',
+    }, function(error, response, body){
+        if (error) {
+            console.log('Error getting stop descriptions: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }else{
+            //console.log('lat: %f, long: %f', JSON.parse(body).results[0].geometry.location.lat, JSON.parse(body).results[0].geometry.location.lng)
+            stopDescs = JSON.parse(body).data
+            //console.log(response.body)
+            console.log("Downloaded stop information")
+            getClosestStopsAllRoutes(routes, src, dest)
+        }
+    })
+}
+
+function getArrivals(routeAndStops, src, dest, mcb){
+
+    request({
+        headers: {
+            'X-Mashape-Key': translocKey,
+            'Accept': 'application/json',
+        },
+        url: 'https://transloc-api-1-2.p.mashape.com/arrival-estimates.json',
+        qs: {
+            routes: routeAndStops.routeId
+        },
+        method: 'GET',
+    }, function(error, response, body){
+        if (error) {
+            console.log('Error getting distance matrix: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }else{
+            var arrivalList = JSON.parse(body).data
+            var numStopsWithArrivals = arrivalList.length
+
+            for(var i = 0 ; i < numStopsWithArrivals; i++){
+                if (arrivalList[i].stop_id == routeAndStops.closestToSrc){
+                    // look at list "arrivals" for this stop
+                    // check if arrival time - current time < walking distance time
+                    // if yes, add new option to list
+                    // containing all the information together with the arrival time
+
+                    var arrivals = arrivalList[i].arrivals
+
+                    arrivals.forEach(function(arrival){
+                        i = i
+                    })
+                }
+            }
+        }
+    })
+}
+
+function getStopArrivalTimes(src, dest){
+
+    // for each route, we have two stops
+    // for the srcStops only, find the arrival times
+    // if no arrival time, don't add to array
+    // at the end, 
+
+    var asyncTasksArrivals = []
+
+    routesAndClosestStops.forEach(function(routeAndStops){
+        asyncTasksArrivals.push(function(callback){
+
+            getArrivals(routeAndStops, src, dest, function(){
+                callback()
+            })
+        })
+    })
+
+    async.parallel(asyncTasksArrivals, function(){
+        // do something next
+    })
+}
+
+function getRouteArrivalTimes(routeId){
+
+}
+
+function getWalkingDistance(src, dest){
+
 }
 
 
